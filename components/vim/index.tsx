847dddb8f34141f50ee03c739f326002023be87b
Piotrek <65945270+Piotrek-hub@users.noreply.github.com>
2023-02-05 14:58:11 +0100

    Mobile

--- components/vim/index.tsx
+++ components/vim/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { MenuChoice } from '../../types/components/bar';
 import VimProps from '../../types/components/vim';
 import Bar from './bar';
@@ -7,11 +7,24 @@ import Navbar from './navbar';
 
 export default function Vim({ defaultView }: VimProps) {
 	const [currentView, setCurrentView] = useState<MenuChoice>(defaultView);
+	const [screenWidth, setScreenWidth] = useState(0);
+
+	useEffect(() => {
+		setScreenWidth(window.innerWidth);
+	}, []);
 	return (
 		<div className="w-full h-[100vh] relative bg-bg0">
-			<Navbar />
-			<Content currentView={currentView} />
-			<Bar setCurrentView={setCurrentView} />
+			{screenWidth > 1280 ? (
+				<>
+					<Navbar />
+					<Content currentView={currentView} />
+					<Bar setCurrentView={setCurrentView} />
+				</>
+			) : (
+				<div className="text-fg0 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full  text-center px-[20px]">
+					Website not available on screen smaller than 1280px
+				</div>
+			)}
 		</div>
 	);
 }
